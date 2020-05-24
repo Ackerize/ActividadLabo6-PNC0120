3b6d098c26ed74b7d8689506d89b9415e60a8728
DavidVallecios <00078217@uca.edu.sv>
2020-05-24 11:25:11 -0600

    [Actividad] Actividad finalizada

---
+++ src/main/java/com/uca/capas/service/EstudianteServiceImpl.java
@@ -0,0 +1,35 @@
+package com.uca.capas.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.stereotype.Service;
+
+import com.uca.capas.dao.EstudianteDAO;
+import com.uca.capas.domain.Estudiante;
+
+@Service
+public class EstudianteServiceImpl implements EstudianteService{
+
+	@Autowired
+	EstudianteDAO estudianteDAO;
+	
+	
+	@Override
+	public List<Estudiante> findAll() throws DataAccessException {
+		return estudianteDAO.findAll();
+	}
+
+	@Override
+	public void save(Estudiante estudiante) throws DataAccessException {
+		estudianteDAO.insert(estudiante);
+		
+	}
+
+	@Override
+	public void delete(Integer codigoEstudiante) throws DataAccessException {
+		estudianteDAO.delete(codigoEstudiante);
+	}
+
+}
